a925bb1f9b18c258026870cb1f108f9ed3e57791
ARKye03 <rafa03-dev@proton.me>
2024-08-07 11:26:05 -0400

    test

--- flake.nix
+++ flake.nix
@@ -32,9 +32,38 @@
           ];
         };
 
+        generateCppProperties = pkgs.stdenv.mkDerivation {
+          name = "generate-cpp-properties";
+          buildInputs = [ pkgs.pkg-config ];
+
+          src = ./.;
+
+          buildCommand = ''
+            mkdir -p $out/.vscode
+            includePaths=$(pkg-config --cflags gtk4 gtk4-layer-shell-0 | tr ' ' '\n' | grep '\-I' | sed 's/-I//g' | jq -R -s -c 'split("\n") | map(select(length > 0))')
+            cat > $out/.vscode/c_cpp_properties.json <<EOF
+            {
+                "configurations": [
+                    {
+                        "name": "Linux",
+                        "includePath": \$includePaths,
+                        "defines": [],
+                        "compilerPath": "/usr/bin/clang",
+                        "cStandard": "c23",
+                        "cppStandard": "c++23",
+                        "intelliSenseMode": "linux-clang-x64"
+                    }
+                ],
+                "version": 4
+            }
+            EOF
+          '';
+        };
+
       in
       {
         devShells.default = shell;
+        packages.${system}.generateCppProperties = generateCppProperties;
       }
     );
 }
