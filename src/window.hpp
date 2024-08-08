929cb63f9eacb2c00ae46c9bbad859293a8652f8
ARKye03 <rafa03-dev@proton.me>
2024-08-07 22:15:25 -0400

    [WIP]

---
+++ src/window.hpp
@@ -0,0 +1,18 @@
+#include <gtkmm.h>
+
+class master_window
+{
+private:
+    /* data */
+public:
+    master_window(/* args */);
+    ~master_window();
+};
+
+master_window::master_window(/* args */)
+{
+}
+
+master_window::~master_window()
+{
+}
